9d39911dfefdd22ff5246fcdbcd8cdf17a99dce5
Sannah Kuip <110873@st-maartenscollege.nl>
2022-03-09 10:04:08 +0100

    contact gebeure,nog net niet helemaal af, bezig met afpoetsen

--- teeheeater/Models/Person.cs
+++ teeheeater/Models/Person.cs
@@ -5,14 +5,19 @@ namespace teeheeater.Models
    public class Person
     
     {
-        [Required(ErrorMessage = "Alstublieft uw voornaam invullen")] 
+        [Required(ErrorMessage = "Gelieve uw voornaam in te vullen")]
+        [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Alstublieft uw achternaam invullen")]
+        [Required(ErrorMessage = "Achternaam is een verplicht veld")]
+        [Display(Name = "Achternaam")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Alstublieft uw e-mail invullen")]
+        [Required(ErrorMessage = "Emailadres is verplicht")]
+        [EmailAddress(ErrorMessage = "Geen geldig email adres")]
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+        [Required(ErrorMessage = "Bericht is verplicht")]
+        [Display(Name = "Bericht")]
         public string Description { get; set; }
     }
 }
